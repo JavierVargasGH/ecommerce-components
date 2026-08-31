4d3ba7997b8639f4feae74de93baad65c2290f64
Javier Vargas <jvb.ici@gmail.com>
2026-08-31 00:27:18 -0400

    Fix: actualizando URLs de las imágenes

--- src/data/products.js
+++ src/data/products.js
@@ -4,42 +4,42 @@ const products = [
         name: "Auriculares Razer Kraken X",
         price: 50000,
         category: "Sonido",
-        image: "https://tinyurl.com/3cydhaat"
+        image: "https://imgs.search.brave.com/J9irsV1gXSa2TsEzAMjo9UQ1De6SoWFpKxBH6rOjIJk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMyLnJhemVyem9u/ZS5jb20vaW1hZ2Vz/L3BueC5hc3NldHMv/YTEyYjZlNjIxN2Uz/ZmEyNzA5OWY2ODBl/MmJiYjM2MDkvcmF6/ZXIta3Jha2VuLXYz/LXgtY2hyb21hLXJn/Yi1tb2JpbGUtNzY4/eDQ2MC5qcGc"
     },
     {
         id: 2,
         name: "Teclado Mecánico Corsair K70",
         price: 80000,
         category: "Teclados",
-        image: "https://tinyurl.com/mvrvyt2j"
+        image: "https://imgs.search.brave.com/YYj6CjJF3OIuC5dbR94IecVVDWj5oEEqafR0VBxAG3A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9ldGNo/aWxlLm5ldC93cC1j/b250ZW50L3VwbG9h/ZHMvMjAyMy8xMS9L/NzBfUFJPX09QWF9Q/QlRfMzAtUGhvdG9S/b29tLnBuZw"
     },
     {
         id: 3,
         name: "Mouse Gamer Razer DeathAdder",
         price: 49000,
         category: "Mouse",
-        image: "https://tinyurl.com/3rxwv3m8"
+        image: "https://imgs.search.brave.com/W_C-KN-AmA2Kr-CDtU8uCu1DMjKJS8gVWvBop7utqCI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMyLnJhemVyem9u/ZS5jb20vaW1hZ2Vz/L3BueC5hc3NldHMv/YTFlZDA1NmU0ZjIy/MTFhYTNjMjgwMmVm/OGQzOTMyMDcvcmF6/ZXItZGVhdGhhZGRl/ci12NC1wcm8tYmln/Z2VzdC10ZWNoLWxl/YXAtZGVza3RvcC12/Mi53ZWJw"
     },
     {
         id: 4,
         name: "Mouse Gamer Razer Naga X",
         price: 80000,
         category: "Mouse",
-        image: "https://tinyurl.com/4fhndhe6"
+        image: "https://imgs.search.brave.com/5IQ2lS7FZf1TGwZK8Zc4FdTaGOYO5GYf98BmyGVqfqU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/bW9zLmNtcy5mdXR1/cmVjZG4ubmV0L0RU/UDJ6YkNuRnVXQ2lO/N3IyU1l0UkUuanBn"
     },
     {
         id: 5,
         name: "Equipo Sonido Razer Leviathan",
         price: 199990,
         category: "Sonido",
-        image: "https://tinyurl.com/3z6926f4"
+        image: "https://imgs.search.brave.com/iMtAQTnPy_lo_Eo9NwZ5NXdFiMKFU30yfLGd-c-rl5I/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMyLnJhemVyem9u/ZS5jb20vaW1hZ2Vz/L3BueC5hc3NldHMv/YjZiMDZiZGM2NjE2/MmVjYzY3ZTUyNzhl/OWI0YzMzMDkvbGV2/aWF0aGFuLXYyLXBy/by1oZXJvLTE5MjB4/NzAwLndlYnA"
     },
     {
         id: 6,
         name: "Monitor Gamer LG 27GL83A-B",
         price: 300000,
         category: "Monitores",
-        image: "https://tinyurl.com/4nz77hzy"
+        image: "https://imgs.search.brave.com/psrgEeEH6oTprfmmm2ONUwnfdIkqNrWFfVJBijcssN4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTFGM0IxdVhoYkwu/anBn"
     }
 ];
 
